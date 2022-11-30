ee4aaa1f97c17ee24bd38a6d95831fd3047561c9
Crazines2 <Adrian.radic@hotmail.com>
2022-11-30 14:12:14 +0100

    Added titles about the random nobel prize info

--- src/NobelPrize.js
+++ src/NobelPrize.js
@@ -4,12 +4,14 @@ import Laureate from "./Laureate";
 
 export default function NobelPrize(props) {
     return (<div>
-        <div> {props.data.awardYear}</div>
-        <div>{props.data.category.en}</div>
+        <div> Award Year: {props.data.awardYear}</div>
+        <div> Category: {props.data.category.en}</div>
+        
         {props.data.laureates === undefined ? 
             <div>price was not given out</div> : 
+            
             props.data.laureates.map((laureate) => {return <Laureate data={laureate} key={laureate.id} />})}
-        <div>{props.data.prizeAmount}</div>
-        <div>{props.data.prizeAmountAdjusted}</div>
+        <div> prizeAmount: {props.data.prizeAmount}</div>
+        <div> prizeAmountAdjusted: {props.data.prizeAmountAdjusted}</div>
     </div>)
 }

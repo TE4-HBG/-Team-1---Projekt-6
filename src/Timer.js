aacaa14aa1a4b792bc187c71df98ce383e438244
KLohger <fanimdude32@gmail.com>
2022-11-30 13:39:54 +0100

    Update Timer.js

--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getCount, get } from "./API";
+import { getCount, get } from "./RequestAPI";
 import NobelPrize from "./NobelPrize";
 
 

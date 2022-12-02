fae75bcc53035187bfe29ed3e9b67fb76fae8acf
KLohger <fanimdude32@gmail.com>
2022-12-02 09:51:43 +0100

    made conn.js use config.env

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,11 +1,10 @@
 import { MongoClient, Db } from "mongodb";
-const scaryString = "mongodb+srv://root:X1X8woKNt4rLtEs2@cluster0.eriyvi2.mongodb.net/?retryWrites=true&w=majority";
 /**
  * @type {MongoClient | null}
  */
 let _client = null;
 export async function connectToServer() {
-    const client = await (new MongoClient(scaryString,   {
+    const client = await (new MongoClient(process.env.ATLAS_URI,   {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).connect());

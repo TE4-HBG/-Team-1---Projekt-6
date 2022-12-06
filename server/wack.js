c29be8a80d4261a2e55583beb6685788518cd6fa
KLohger <fanimdude32@gmail.com>
2022-12-06 09:26:51 +0100

    added some documentation for replaceOne, to make it more swag

--- server/wack.js
+++ server/wack.js
@@ -1,3 +1,12 @@
+import {UpdateResult, Document, Collection } from "mongodb";
+/**
+ * @param {Collection<Document>} collection
+ * @param {number} index
+ * @param {*} object
+ * @param {(object : *) => *} translateIntoDatabaseStructure
+ * @param {(object : *) => number | undefined} getId
+ * @returns {Promise<UpdateResult | Document>}
+ */
 export default function replaceOne(collection, index, object, translateIntoDatabaseStructure, getId) {
     return collection.replaceOne({_id: (getId ? getId(object) : index) }, translateIntoDatabaseStructure(object), {upsert:true})
 }

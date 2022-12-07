489ebff69db022344605be5e9008a9d4f8f8c652
Crazines2 <Adrian.radic@hotmail.com>
2022-12-07 14:14:37 +0100

    PopUp
    Styling is needed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import './App.css';
-import {ApiTest} from "./ApiTest"
-import {Searchbar} from "./Searchbar"
-import {HandleSearch, ReturnObjectIdOfLettersPrompt} from './handleSearch.js'
+import { ApiTest } from "./ApiTest"
+import { Searchbar } from "./Searchbar"
+import { HandleSearch, ReturnObjectIdOfLettersPrompt } from './handleSearch.js'
 import { useEffect, useState } from 'react';
 import { getCount, get } from "./RequestAPI";
 import Container from 'react-bootstrap/Container';
@@ -10,13 +10,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Timer from "./Timer"
 import NobelPrize from "./NobelPrize";
+import Popup from './components/Modal/Popup';
+
 
 function App() {
   const [prompt, setPrompt] = useState("")
   const [nobelPrizes, setNobelPrizes] = useState(null);
-  const [count, setCount] = useState(null);  
+  const [count, setCount] = useState(null);
   const [searchResult, setSearchResult] = useState(null);
-
+  const [popupState, setPopupState] = useState(false);
   let queriedPrizes;
   useEffect(() => {
     getCount(setCount);
@@ -24,46 +26,58 @@ function App() {
 
   useEffect(() => {
     if (count !== null) {
-       get(0, count, setNobelPrizes);
+      get(0, count, setNobelPrizes);
     }
   }, [count]);
 
   useEffect(() => {
-    
-        if (prompt.length > 0) {
-          setSearchResult(ReturnObjectIdOfLettersPrompt(nobelPrizes, prompt))
-          console.log("search result set")
-          /*console.log(nobelPrizes)
-         
-          queriedPrizes += nobelPrizes.filter((nobelPrize) => {
-          return;
-          });*/
-        }
-      
+
+    if (prompt.length > 0) {
+      setSearchResult(ReturnObjectIdOfLettersPrompt(nobelPrizes, prompt))
+      console.log("search result set")
+      /*console.log(nobelPrizes)
+     
+      queriedPrizes += nobelPrizes.filter((nobelPrize) => {
+      return;
+      });*/
+    }
+
   }, [prompt])
-  
+
   console.log(searchResult);
   return (
     <div className="App">
-      
-      <NavbarDarkExample />
-      <Searchbar updatePrompt = {setPrompt} />
-      <HandleSearch prompt = {prompt} lauretaes = {queriedPrizes} />
-      <Timer/>
-      <div className="Test"> 
-      {
-        searchResult !== null && searchResult.map((index) => {return <NobelPrize index = {index} />})
-      }
+
+      <NavbarDarkExample loginClick={function () { setPopupState(!popupState) }} />
+      <Searchbar updatePrompt={setPrompt} />
+      <HandleSearch prompt={prompt} lauretaes={queriedPrizes} />
+      <Timer />
+      <div className="Test">
+        {
+          searchResult !== null && searchResult.map((index) => { return <NobelPrize index={index} /> })
+        }
       </div>
+      <Popup state={popupState} setState={setPopupState} >
+        <h2>
+          HELLO WORLD!
+        </h2>
+      </Popup>
+
     </div>
   );
 }
 
-export function NavbarDarkExample() {
+export function NavbarDarkExample(props) {
+
   return (
     <Navbar variant="dark" bg="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#home">NOBELFINDER</Navbar.Brand>
+        <Navbar.Brand className="Login" href="#home">NOBELFINDER</Navbar.Brand>
+        <Navbar.Brand>
+          <button onClick={props.loginClick}>
+            Login!
+      </button>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-dark-example" />
         <Navbar.Collapse id="navbar-dark-example">
           <Nav>
@@ -80,11 +94,14 @@ export function NavbarDarkExample() {
                 TEAM 1 PRODUCTION
               </NavDropdown.Item>
             </NavDropdown>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
   );
 }
+
 
 export default App;
---
+++ src/components/Modal/Popup.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./Modal.css";
+
+export default function Popup(props) {
+  
+  const toggleModal = () => {
+    props.setState(!props.state);
+  };
+  if(props.state) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+  return (
+    <>
+      {props.state && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            {props.children}
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
